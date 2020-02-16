6b99cf6a3a4facee86c1983c04944600a9cd6939
Bnaya Peretz <me@bnaya.net>
2020-02-16 16:55:28 +0200

    wip

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,11 @@
 /* eslint-env node */
 // https://babeljs.io/docs/en/options#babelrcroots
-
-const path = require("path");
-
-// console.log(path.resolve(__dirname, "./packages/*"));
-
 module.exports = {
   babelrcRoots: [
     // Keep the root as a root
     ".",
 
     // Also consider monorepo packages "root" and load their .babelrc.json files.
-    // path.resolve(__dirname, "./packages/*"),
     "./packages/*"
   ]
 };
